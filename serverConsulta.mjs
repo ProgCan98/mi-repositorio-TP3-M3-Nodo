6b9070776c9ac3c9fda7e1a1c83d0b8bdeec1609
unknown <cancliniprogramming@gmail.com>
2024-10-24 20:46:49 -0300

    segundo commit tp3

---
+++ serverConsulta.mjs
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/profile', (req, res) => {
+  const edad = req.query.edad;
+  console.log(`edad recibida ${edad}`);
+  res.send(`Edad del perfil: ${edad}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
